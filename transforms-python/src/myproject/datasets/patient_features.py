9986a4c2c0707627f7625709535d4a50b144f48a
Hadi Modarres <hadi.modarres@nhs.net>
2023-06-28 14:55:13 +0000

    Merge: Adding features from patient health record to patient_features table (ri.pull-request.main.pull-request.8e4c24c6-f5b9-4221-9c63-b140ce0bab90)

--- transforms-python/src/myproject/datasets/patient_features.py
+++ transforms-python/src/myproject/datasets/patient_features.py
@@ -2,24 +2,61 @@
 from transforms.api import transform_df, Input, Output
 import pyspark.sql.functions as F
 from myproject.datasets import utils
-from myproject.datasets.config import list_categorical_variables, list_boolean_variables
+from myproject.datasets.config import (list_categorical_variables,
+                                       list_boolean_variables,
+                                       list_patient_health_record_variables,
+                                       list_categorical_variables_from_phr)
 
 @transform_df(
     Output("ri.foundry.main.dataset.36644003-34c3-43d0-bace-751b3e071ea3"),
     patient=Input("ri.foundry.main.dataset.b2a84252-8ae1-4f7c-9948-c7e00afe36a8"),
-    dataset_111_calls=Input("ri.foundry.main.dataset.a3dfadbc-b022-4d88-98c2-8ee3392beed1")
+    dataset_111_calls=Input("ri.foundry.main.dataset.a3dfadbc-b022-4d88-98c2-8ee3392beed1"),
+    patient_health_record_features=Input("ri.foundry.main.dataset.195dd683-e55b-450e-abeb-5f00de85ad78")
 )
-def compute(patient, dataset_111_calls):
+def compute(patient, dataset_111_calls, patient_health_record_features):
     """
     Add features to the patient table from the Person Ontology
     Features to be added: demographic, geographic, GP practice
 
     Merge features from 111 calls
+    Merge features from patient health record features
     """
+    # 111 CALLS
+
     # left join 111 dataset
     patient = patient.join(dataset_111_calls,
                            "patient_pseudo_id",
                            "left")
+
+    # PATIENT HEALTH RECORD
+
+    # select relevant columns from patient health record table
+    all_columns_phr = patient_health_record_features.columns
+
+    # select all the ID, columns listed in list_patient_health_record_variables, and the dummy variables created from 
+    columns_to_select_from_phr = ["patient_pseudo_id"] + list_patient_health_record_variables
+
+    # add all of the binary columns created from the categorical columns
+    for col in all_columns_phr:
+        for cat_var in list_categorical_variables_from_phr:
+            if cat_var + "_" in col:
+                columns_to_select_from_phr.append(col)
+
+    patient_health_record_features_selected = patient_health_record_features.select(columns_to_select_from_phr)
+
+    # rename the columns to indicate their source
+    for col in patient_health_record_features_selected.columns:
+        # we want to keep ID column name as it is
+        if col != "patient_pseudo_id":
+            patient_health_record_features_selected = patient_health_record_features_selected.withColumnRenamed(col,
+                                                                                                               "phr_"+col)
+
+    # left join patient health record features tables
+    patient = patient.join(patient_health_record_features_selected,
+                           "patient_pseudo_id",
+                           "left")
+
+    # PATIENT FEATURES
 
     # Age column: replace ages over 113 with None
     patient = patient.withColumn("age_clean", F.when(patient.age <= 113, patient.age)\
--- transforms-python/src/myproject/datasets/patient_health_record_features.py
+++ transforms-python/src/myproject/datasets/patient_health_record_features.py
@@ -2,6 +2,8 @@
 from transforms.api import transform_df, Input, Output
 import pyspark.sql.functions as F
 from myproject.datasets import utils
+from pyspark.sql.window import Window
+from myproject.datasets.config import list_categorical_variables_from_phr
 
 @transform_df(
     Output("/NHS/cancer-late-presentation/cancer-late-datasets/interim-datasets/patient_health_record_features"),
@@ -9,17 +11,22 @@ from myproject.datasets import utils
 )
 def compute(patient_health_record):
     """
-    Add features to the patient health record table   
+    Identify latest row from patient health record table
+    Add features to the patient health record table
     """
+
+    # identifying latest entry. This code should be adapted if the PHR at a different time point/snapshot is required
+    w_desc = Window.partitionBy("patient_pseudo_id").orderBy(F.col("date").desc())
+    patient_health_record_latest = patient_health_record.withColumn("row", F.row_number().over(w_desc)).filter(F.col("row") == 1).drop("row")
 
     all_col_expressions = []
 
-    for cat_variable in ["nhs_region", "stp_code"]:
-        patient_health_record = utils.clean_column_of_strings(patient_health_record, col_name=cat_variable, append_to_col_name="_null_removed", fill_string='unknown')
-        expression_cols = utils.create_expressions_categorical_column(patient_health_record, col_name=cat_variable+"_null_removed")
+    for cat_variable in list_categorical_variables_from_phr:
+        patient_health_record_latest = utils.clean_column_of_strings(patient_health_record_latest, col_name=cat_variable, append_to_col_name="_null_removed", fill_string='unknown')
+        expression_cols = utils.create_expressions_categorical_column(patient_health_record_latest, col_name=cat_variable+"_null_removed")
         all_col_expressions = all_col_expressions + expression_cols
 
-    # Select all columns and add the expressions for the binary columns to be created  
-    patient_health_record = patient_health_record.select(F.col("*"), *all_col_expressions)
+    # Select all columns and add the expressions for the binary columns to be created
+    patient_health_record_latest = patient_health_record_latest.select(F.col("*"), *all_col_expressions)
 
-    return patient_health_record
+    return patient_health_record_latest
